9b6e7841956b3ae511ecf42a20d0bedcb92689c3
Georgi Dimitrov <dgeorgi@vmware.com>
2019-07-25 22:53:20 +0300

    RecipeApp progress - added lombok

--- 04_Spring5RecipeApp/src/main/java/guru/springframework/spring5recipeapp/services/RecipeServiceImpl.java
+++ 04_Spring5RecipeApp/src/main/java/guru/springframework/spring5recipeapp/services/RecipeServiceImpl.java
@@ -2,12 +2,14 @@ package guru.springframework.spring5recipeapp.services;
 
 import guru.springframework.spring5recipeapp.domain.Recipe;
 import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -20,6 +22,8 @@ public class RecipeServiceImpl implements RecipeService {
 
    @Override
    public Set<Recipe> getRecipes() {
+      log.debug("Service Impl Log Debug");
+
       Set<Recipe> recipeSet = new HashSet<>();
       recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
       return recipeSet;
